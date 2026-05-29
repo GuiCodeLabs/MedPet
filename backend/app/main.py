bca51ea95be53e83c64b1dc35ea1cc0117daae30
Pedro Henrique <pedrox.h15z@gmail.com>
2026-05-28 23:34:10 -0300

    :sparkles: feat: registra rotas de clientes e pets no main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.database import Base, engine
-from app.routes import usuario_routes, auth_routes
+from app.routes import usuario_routes, auth_routes, cliente_routes, pet_routes
 from app.models import usuario, cliente, pet, atendimento
 
 app = FastAPI(title="MEDPET - Clínica Veterinária")
@@ -10,6 +10,8 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(usuario_routes.router)
 app.include_router(auth_routes.router)
+app.include_router(cliente_routes.router)
+app.include_router(pet_routes.router)
 
 
 @app.get("/")
